1ada6dd1c196cffdb75bbfd6d49b8ed7554de810
charden <licht1017@gmail.com>
2022-07-01 21:47:05 +0900

    feat: 🎸 写真一覧と写真アップロード画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
-
+  resources :photos, only: %i[index new create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
---
+++ test/integration/photos_test.rb
@@ -0,0 +1,100 @@
+require "test_helper"
+
+class PhotosTest < ActionDispatch::IntegrationTest
+  setup do
+    post "/login", params: { name: "test", password: "password" }
+  end
+
+  test "写真一覧が表示できること" do
+    get "/"
+    assert_response :success
+  end
+
+  test "h1に写真一覧と記載されていること" do
+    get "/"
+    assert_select "h1", "写真一覧"
+  end
+
+  test "｢ログアウト｣リンクがあること" do
+    get "/"
+    assert_select "a.logout-link", "ログアウト"
+  end
+
+  test "｢写真の選択してアップロード｣リンクがあること" do
+    get "/"
+    assert_select "a.photo-upload-link", "写真の選択してアップロード"
+  end
+
+  test "画像が2つ表示されること" do
+    get "/"
+    assert_select ".photo", 2
+  end
+
+  test "投稿したタイトル｢one｣が表示されること" do
+    get "/"
+    assert_select ".photoLabel", "one"
+  end
+
+  test "投稿したタイトル｢two｣が表示されること" do
+    get "/"
+    assert_select ".photoLabel", "two"
+  end
+
+  test "画像をアップロードすると追加されること" do
+    post "/photos", params: { photo: {title: "test", image: Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'files', 'three.png'), 'image/png') }}
+    follow_redirect!
+    assert_select ".photoLabel", "test"
+    assert_select ".photo", 3
+  end
+
+  test "h1に写真アップロードと記載されていること" do
+    get "/photos/new"
+    assert_select "h1", "写真アップロード"
+  end
+
+  test "タイトルの入力欄があること" do
+    get "/photos/new"
+    assert_select "form input[name='photo[title]']"
+  end
+
+  test "ファイルの選択欄があること" do
+    get "/photos/new"
+    assert_select "form input[type=file]"
+  end
+
+  test "formのsubmitボタンがアップロードになっていること" do
+    get "/photos/new"
+    assert_select "form input[type=submit][value=?]", "アップロード"
+  end
+
+  test "フォームのPOST先が/photosであること" do
+    get "/photos/new"
+    assert_select "form[action=?]", "/photos"
+  end
+
+  test "キャンセルリンクがあること" do
+    get "/photos/new"
+    assert_select "a.photos-link", "キャンセル"
+  end
+
+  test "タイトルが未入力の場合に｢ユーザーIDを入力してください｣と表示されること" do
+    post "/photos", params: { photo: {title: "", image: Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'files', 'three.png'), 'image/png') }}
+    assert_select "ul > li", "タイトルを入力してください"
+  end
+
+  test "タイトルが30文字以上の場合に｢タイトルは30文字以内で入力してください｣と表示されること" do
+    post "/photos", params: { photo: {title: "この文章はダミーです。文字の大きさ、量、字間、行間等を確認する", image: Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'files', 'three.png'), 'image/png') }}
+    assert_select "ul > li", "タイトルは30文字以内で入力してください"
+  end
+
+  test "画像が未選択の場合に｢画像ファイルを入力してください｣と表示されること" do
+    post "/photos", params: { photo: {title: "test", image: nil}}
+    assert_select "ul > li", "画像ファイルを入力してください"
+  end
+
+  test "未ログイン時写真アップロード画面にアクセスするとログイン画面にリダイレクトされること" do
+    get "/logout"
+    get "/photos/new"
+    assert_redirected_to login_path
+  end
+end
